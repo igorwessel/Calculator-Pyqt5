5f17101c339f6a5a2758eb03661ae7dc78f811ca
igorwessel <igor_wessel@hotmail.com>
2020-02-21 14:19:15 -0300

    put buttons in layout

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Calculator(QMainWindow):
         self.calcLayout = QGridLayout(self.main)
         self.setCentralWidget(self.main)
 
-        # LAYOUT
+        # layout
 
         self.display_calc = QLineEdit()
         self.display_calc.setDisabled(True)
@@ -20,8 +20,25 @@ class Calculator(QMainWindow):
             QSizePolicy.Preferred, QSizePolicy.Expanding)
         self.main.setLayout(self.calcLayout)
 
-        def add_btn(self, btn, row, col, rowspan, colspan):
-            pass
+        # numbers buttons
+        self.add_btn(QPushButton('7'), 1, 0, 1, 1)
+        self.add_btn(QPushButton('8'), 1, 1, 1, 1)
+        self.add_btn(QPushButton('9'), 1, 2, 1, 1)
+        self.add_btn(QPushButton('4'), 2, 0, 1, 1)
+        self.add_btn(QPushButton('5'), 2, 1, 1, 1)
+        self.add_btn(QPushButton('6'), 2, 2, 1, 1)
+        self.add_btn(QPushButton('1'), 3, 0, 1, 1)
+        self.add_btn(QPushButton('2'), 3, 1, 1, 1)
+        self.add_btn(QPushButton('3'), 3, 2, 1, 1)
+
+        # operations mathematics
+        self.add_btn(QPushButton('x'), 1, 3, 1, 1)
+        self.add_btn(QPushButton('+'), 2, 3, 1, 1)
+        self.add_btn(QPushButton('-'), 3, 3, 1, 1)
+
+    def add_btn(self, btn, row, col, rowspan, colspan):
+        self.calcLayout.addWidget(btn, row, col, rowspan, colspan)
+        btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
 
 
 if __name__ == '__main__':
