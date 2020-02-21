5f9d0002f2fcfbdad6cafb427accf3d23895e71a
igorwessel <igor_wessel@hotmail.com>
2020-02-21 11:50:51 -0300

    changes in layout, making function for add buttons in layout

--- main.py
+++ main.py
@@ -1,50 +1,27 @@
 from PyQt5.QtWidgets import (
-    QMainWindow, QApplication, QGridLayout, QLineEdit, QPushButton, QWidget)
+    QMainWindow, QApplication, QGridLayout, QLineEdit, QPushButton, QWidget, QSizePolicy)
 
 
 class Calculator(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle('Calculator')
-        self.resize(400, 400)
-
-        # LAYOUT
-        self.main = QWidget(self)
+        self.setFixedSize(400, 400)
+        self.main = QWidget()
+        self.calcLayout = QGridLayout(self.main)
         self.setCentralWidget(self.main)
 
-        calcLayout = QGridLayout()
-        display_calc = QLineEdit()
-        calcLayout.addWidget(display_calc, 0, 0)
+        # LAYOUT
 
-        btn_0 = QPushButton('0')
-        btn_sum = QPushButton('+')
-        btn_something = QPushButton('')
-        btn_dot = QPushButton('.')
-        btn_subtract = QPushButton('-')
-        btn_1 = QPushButton('1')
-        btn_2 = QPushButton('2')
-        btn_3 = QPushButton('3')
-        btn_4 = QPushButton('4')
-        btn_5 = QPushButton('5')
-        btn_6 = QPushButton('6')
-        btn_7 = QPushButton('7')
-        btn_8 = QPushButton('8')
-        btn_9 = QPushButton('9')
+        self.display_calc = QLineEdit()
+        self.display_calc.setDisabled(True)
+        self.calcLayout.addWidget(self.display_calc, 0, 0, 1, 5)
+        self.display_calc.setSizePolicy(
+            QSizePolicy.Preferred, QSizePolicy.Expanding)
+        self.main.setLayout(self.calcLayout)
 
-        calcLayout.addWidget(btn_7, 1, 0)
-        calcLayout.addWidget(btn_8, 1, 1)
-        calcLayout.addWidget(btn_9, 1, 2)
-        calcLayout.addWidget(btn_4, 2, 0)
-        calcLayout.addWidget(btn_5, 2, 1)
-        calcLayout.addWidget(btn_6, 2, 2)
-        calcLayout.addWidget(btn_1, 3, 0)
-        calcLayout.addWidget(btn_2, 3, 1)
-        calcLayout.addWidget(btn_3, 3, 2)
-        calcLayout.addWidget(btn_0, 4, 1)
-        calcLayout.addWidget(btn_something, 4, 0)
-        calcLayout.addWidget(btn_dot, 4, 2)
-
-        self.main.setLayout(calcLayout)
+        def add_btn(self, btn, row, col, rowspan, colspan):
+            pass
 
 
 if __name__ == '__main__':
